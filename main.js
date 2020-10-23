3844b527c360445eae4dcf47bdf8d3a1db348339
lindsay-greene <lindsay.smile@yahoo.com>
2020-10-22 20:45:28 -0400

    made position arrays into lookup table

--- main.js
+++ main.js
@@ -257,12 +257,16 @@ function ShowPosition (evt) {
 // compare each articulator to itself to check if it still needs to be changed using d attribute (current state vs end state)
 // if end state 
 var newAnim = null;
-function animateAll() {
+function animateAll(place, voice) {
   var changed = false;
-  animations = ["Jaw", "Palette", "VocalFolds", "Cartilage", "Tongue"]
-  animations.forEach((animation) => {
-    console.log(animation);
-    var current = animation.getAttribute("d");
+  articulators = ["jaw", "palette", "tongue", "vocalfolds", "cartilage"];
+  articulators.forEach((articulator) => {
+    var articulator_el = document.getElementById(articulator);
+    console.log(articulator_el);
+    var current = articulator_el.getAttribute("d");
+    console.log("current " + current);
+    newAnim = articulatorLookup[articulator];
+    console.log(newAnim);
     newAnim = newAnim.replace(/,/g, " ");
     current = current.replace(/,/g, " ");
 
@@ -285,56 +289,59 @@ function animateAll() {
             eachDescNum--;
             changed = true;
           }
+          // TODO: changed has to be false at some point (when )
 
+          // if(changed) break; ?? Because if just one is true then we need to call animateAll
+          // if(current != newAnim)
           numberArray[n] = command + eachDescNum;
         }
       }
 
       var tempDesc = numberArray.join(" ");
-      animation.setAttribute("d", tempDesc);
-      // ONly call animate all once at end 
-      if (changed) {
-        window.setTimeout(animateAll());
-      }
+      articulator_el.setAttribute("d", tempDesc);
     }
   });
+  if (changed) {
+    window.setTimeout(animateAll());
+  }
 } 
 
 async function animateSound (place, voice) {
-  var vocalFoldsDesc = vocalFoldsArray[voice];
-  newVocalFolds = vocalFoldsDesc;
+  animateAll(place, voice);
+  // var vocalFoldsDesc = vocalFoldsArray[voice];
+  // newVocalFolds = vocalFoldsDesc;
 
-  var cartilageDesc = cartilageArray[voice];
-  newCartilage = cartilageDesc;
+  // var cartilageDesc = cartilageArray[voice];
+  // newCartilage = cartilageDesc;
 
-  AnimateVocalFolds();
-  AnimateCartilage();
+  // AnimateVocalFolds();
+  // AnimateCartilage();
 
-  var jawPosition = place;
-  if (!jawArray[place]) {
-    jawPosition = "rest";
-  }
-  var jawDesc = jawArray[jawPosition];
+  // var jawPosition = place;
+  // if (!jawArray[place]) {
+  //   jawPosition = "rest";
+  // }
+  // var jawDesc = jawArray[jawPosition];
 
-  var palettePosition = place;
-  if (!paletteArray[place]) {
-    palettePosition = "pharyngeal";
-  }
-  var paletteDesc = paletteArray[palettePosition];
+  // var palettePosition = place;
+  // if (!paletteArray[place]) {
+  //   palettePosition = "pharyngeal";
+  // }
+  // var paletteDesc = paletteArray[palettePosition];
 
-  var tonguePosition = place;
-  if (!tongueArray[place]) {
-    tonguePosition = "rest";
-  }
-  var tongueDesc = tongueArray[tonguePosition];
+  // var tonguePosition = place;
+  // if (!tongueArray[place]) {
+  //   tonguePosition = "rest";
+  // }
+  // var tongueDesc = tongueArray[tonguePosition];
 
-  newTongue = tongueDesc;
-  AnimateTongue();
+  // newTongue = tongueDesc;
+  // AnimateTongue();
 
-  newJaw = jawDesc;
-  AnimateJaw();
-  newPalette = paletteDesc;
-  AnimatePalette();
+  // newJaw = jawDesc;
+  // AnimateJaw();
+  // newPalette = paletteDesc;
+  // AnimatePalette();
 };
 
 // async function AnimateMouth(mouthPart, currentPos, newPos) {
@@ -565,34 +572,124 @@ async function AnimateCartilage() {
   }
 };
 
-var tongueArray = new Array();
-tongueArray["rest"] = "M159,283 C179,254 128,235 91,243 S59,264 86,280 ";
-tongueArray["dental"] = "M159,283 C173,248 129,246 85,241 S58,259 86,280";
-tongueArray["interdental"] = "M159,283 C174,244 130,236 79,250 S67,259 86,280";
-tongueArray["alveolar"] = "M159,283 C174,244 131,254 87,242 S68,254 86,280";
-tongueArray["alveopalatal"] = "M159,283 C145,247 91,206 74,246 S103,224 86,280";
-tongueArray["palatal"] = "M159,283 C177,213 113,214 87,248 S107,247 86,280";
-tongueArray["velar"] = "M159,283 C158,162 126,236 88,249 S93,261 86,280";
-tongueArray["uvular"] = "M159,283 C183,177 147,230 94,249 S93,261 86,280";
-tongueArray["pharyngeal"] = "M159,283 C204,257 196,219 120,246 S109,261 86,280";
+// Make into lookup tables 
+const articulatorLookup = {
+  "tongue" : {
+      "rest": {
+        "path":"M159,283 C179,254 128,235 91,243 S59,264 86,280 "
+      },
+      "dental": {
+        "path":"M159,283 C173,248 129,246 85,241 S58,259 86,280"
+      },
+      "interdental": {
+        "path":"M159,283 C174,244 130,236 79,250 S67,259 86,280"
+      },
+      "alveolar": {
+        "path":"M159,283 C174,244 131,254 87,242 S68,254 86,280"
+      },
+      "alveopalatal": {
+        "path":"M159,283 C145,247 91,206 74,246 S103,224 86,280"
+      },
+      "palatal": {
+        "path":"M159,283 C177,213 113,214 87,248 S107,247 86,280"
+      },
+      "velar": {
+        "path":"M159,283 C158,162 126,236 88,249 S93,261 86,280"
+      },
+      "uvular": {
+        "path":"M159,283 C183,177 147,230 94,249 S93,261 86,280"
+      },
+      "pharyngeal": {
+        "path":"M159,283 C204,257 196,219 120,246 S109,261 86,280"
+      }
+  },
+  "jaw" : {
+  
+      "rest": {
+        "path":"M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,283 64,270 T61,283 C49,279 54,259 38,267 S44,285 39,301 C27,352 55,341 101,340 S131,364 136,375 Q143,399 140,420"
+      },
+      "bilabial": {
+        "path":"M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,278 64,262 T60,274 C47,273 53,251 37,256 S41,268 37,291 C26,342 55,332 101,340 S131,364 136,375 Q143,399 140,420"
+      },
+      "labiodental": {
+        "path":"M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,278 64,262 T60,274 C47,273 70,262 49,254 S47,267 37,291 C26,342 55,332 101,340 S131,364 136,375 Q143,399 140,420"
+      },
+      "pharyngeal": {
+        "path":"M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 88,278 Q70,298 63,280 T60,290 C48,288 54,268 38,276 S44,294 39,310 C27,361 55,350 99,349 S131,364 136,375 Q143,399 140,420"
+      }
+  },
+  
+  "palette" : {
+  
+      "rest": {
+        "path":"M30,221 C119,224 140,201 166,207 C177,223 173,245 163,228 S144,220 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221"
+      },
+      "glottal": {
+        "path":"M30,221 C119,224 140,201 166,207 C177,223 173,245 163,228 S144,220 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221"
+      },
+      "bilabial": {
+        "path":"M30,221 C118,220 140,199 175,211 C182,232 182,254 170,230 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C43,241 48,258 34,255 S36,241 29,221"
+      },
+      "labiodental": {
+        "path":"M30,221 C118,220 138,193 170,203 C179,221 178,245 165,222 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C43,229 48,258 33,250 S37,238 29,221"
+      },
+      "pharyngeal": {
+        "path":"M30,221 C118,220 138,193 170,203 C179,221 178,245 165,222 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221"
+      }
+  },
+  
+  "vocalFolds" : {
+  
+      "voiceless": {
+        "path":"M270,309 Q281,334 284,364 L295,360 Q291,324 275,301 H265 Q249,324 245,360 L256,364 Q259,334 270,309"
+      },
+      "voiced": {
+        "path":"M270,309 Q271,334 271,371 L283,364 Q285,324 275,301 H265 Q254,324 257,364 L269,371 Q269,334 270,309"
+      }
+  },
+  
+  "cartilage" : {
+      "voiceless": {
+        "path":"M225,322 L230,339 Q243,347 250,359 262,364 270,371 279,364 290,359 298,347 310,339 L314,322 320,326 315,339 Q310,354 298,364 288,378 270,374 252,378 242,364 230,354 225,339 L219,326 Z"
+      },
+      "voiced": {
+        "path":"M225,322 L230,339 Q246,351 250,359 263,359 270,371 278,359 290,359 295,351 310,339 L314,322 320,326 315,339 Q309,360 298,364 288,379 270,374  252,379 242,364 231,360 225,339 L219,326 Z"
+      }
+  }
+};
 
-var jawArray = new Array();
-jawArray["rest"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,283 64,270 T61,283 C49,279 54,259 38,267 S44,285 39,301 C27,352 55,341 101,340 S131,364 136,375 Q143,399 140,420";
-jawArray["bilabial"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,278 64,262 T60,274 C47,273 53,251 37,256 S41,268 37,291 C26,342 55,332 101,340 S131,364 136,375 Q143,399 140,420";
-jawArray["labiodental"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,278 64,262 T60,274 C47,273 70,262 49,254 S47,267 37,291 C26,342 55,332 101,340 S131,364 136,375 Q143,399 140,420";
-jawArray["pharyngeal"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 88,278 Q70,298 63,280 T60,290 C48,288 54,268 38,276 S44,294 39,310 C27,361 55,350 99,349 S131,364 136,375 Q143,399 140,420";
 
-var paletteArray = new Array();
-paletteArray["rest"] = "M30,221 C119,224 140,201 166,207 C177,223 173,245 163,228 S144,220 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221";
-paletteArray["glottal"] = "M30,221 C119,224 140,201 166,207 C177,223 173,245 163,228 S144,220 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221";
-paletteArray["bilabial"] = "M30,221 C118,220 140,199 175,211 C182,232 182,254 170,230 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C43,241 48,258 34,255 S36,241 29,221";
-paletteArray["labiodental"] = "M30,221 C118,220 138,193 170,203 C179,221 178,245 165,222 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C43,229 48,258 33,250 S37,238 29,221";
-paletteArray["pharyngeal"] = "M30,221 C118,220 138,193 170,203 C179,221 178,245 165,222 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221";
 
-var vocalFoldsArray = new Array();
-vocalFoldsArray["voiceless"] = "M270,309 Q281,334 284,364 L295,360 Q291,324 275,301 H265 Q249,324 245,360 L256,364 Q259,334 270,309";
-vocalFoldsArray["voiced"] = "M270,309 Q271,334 271,371 L283,364 Q285,324 275,301 H265 Q254,324 257,364 L269,371 Q269,334 270,309";
+// var tongueArray = new Array();
+// // Each should have its own object containing path position and text description 
+// // Use text description to be accessed by screen reader 
+// tongueArray["rest"] = "M159,283 C179,254 128,235 91,243 S59,264 86,280 ";
+// tongueArray["dental"] = "M159,283 C173,248 129,246 85,241 S58,259 86,280";
+// tongueArray["interdental"] = "M159,283 C174,244 130,236 79,250 S67,259 86,280";
+// tongueArray["alveolar"] = "M159,283 C174,244 131,254 87,242 S68,254 86,280";
+// tongueArray["alveopalatal"] = "M159,283 C145,247 91,206 74,246 S103,224 86,280";
+// tongueArray["palatal"] = "M159,283 C177,213 113,214 87,248 S107,247 86,280";
+// tongueArray["velar"] = "M159,283 C158,162 126,236 88,249 S93,261 86,280";
+// tongueArray["uvular"] = "M159,283 C183,177 147,230 94,249 S93,261 86,280";
+// tongueArray["pharyngeal"] = "M159,283 C204,257 196,219 120,246 S109,261 86,280";
 
-var cartilageArray = new Array();
-cartilageArray["voiceless"] = "M225,322 L230,339 Q243,347 250,359 262,364 270,371 279,364 290,359 298,347 310,339 L314,322 320,326 315,339 Q310,354 298,364 288,378 270,374 252,378 242,364 230,354 225,339 L219,326 Z";
-cartilageArray["voiced"] = "M225,322 L230,339 Q246,351 250,359 263,359 270,371 278,359 290,359 295,351 310,339 L314,322 320,326 315,339 Q309,360 298,364 288,379 270,374  252,379 242,364 231,360 225,339 L219,326 Z";
+// var jawArray = new Array();
+// jawArray["rest"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,283 64,270 T61,283 C49,279 54,259 38,267 S44,285 39,301 C27,352 55,341 101,340 S131,364 136,375 Q143,399 140,420";
+// jawArray["bilabial"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,278 64,262 T60,274 C47,273 53,251 37,256 S41,268 37,291 C26,342 55,332 101,340 S131,364 136,375 Q143,399 140,420";
+// jawArray["labiodental"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 86,280 Q69,278 64,262 T60,274 C47,273 70,262 49,254 S47,267 37,291 C26,342 55,332 101,340 S131,364 136,375 Q143,399 140,420";
+// jawArray["pharyngeal"] = "M175,418 C152,370 155,346 177,305 S172,299 163,298 C183,273 161,277 159,282 Q117,264 88,278 Q70,298 63,280 T60,290 C48,288 54,268 38,276 S44,294 39,310 C27,361 55,350 99,349 S131,364 136,375 Q143,399 140,420";
+
+// var paletteArray = new Array();
+// paletteArray["rest"] = "M30,221 C119,224 140,201 166,207 C177,223 173,245 163,228 S144,220 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221";
+// paletteArray["glottal"] = "M30,221 C119,224 140,201 166,207 C177,223 173,245 163,228 S144,220 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221";
+// paletteArray["bilabial"] = "M30,221 C118,220 140,199 175,211 C182,232 182,254 170,230 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C43,241 48,258 34,255 S36,241 29,221";
+// paletteArray["labiodental"] = "M30,221 C118,220 138,193 170,203 C179,221 178,245 165,222 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C43,229 48,258 33,250 S37,238 29,221";
+// paletteArray["pharyngeal"] = "M30,221 C118,220 138,193 170,203 C179,221 178,245 165,222 S126,225 90,228 Q76,238 63,240 C55,243 58,261 55,256 S51,247 48,236 C44,243 48,258 34,254 S37,238 29,221";
+
+// var vocalFoldsArray = new Array();
+// vocalFoldsArray["voiceless"] = "M270,309 Q281,334 284,364 L295,360 Q291,324 275,301 H265 Q249,324 245,360 L256,364 Q259,334 270,309";
+// vocalFoldsArray["voiced"] = "M270,309 Q271,334 271,371 L283,364 Q285,324 275,301 H265 Q254,324 257,364 L269,371 Q269,334 270,309";
+
+// var cartilageArray = new Array();
+// cartilageArray["voiceless"] = "M225,322 L230,339 Q243,347 250,359 262,364 270,371 279,364 290,359 298,347 310,339 L314,322 320,326 315,339 Q310,354 298,364 288,378 270,374 252,378 242,364 230,354 225,339 L219,326 Z";
+// cartilageArray["voiced"] = "M225,322 L230,339 Q246,351 250,359 263,359 270,371 278,359 290,359 295,351 310,339 L314,322 320,326 315,339 Q309,360 298,364 288,379 270,374  252,379 242,364 231,360 225,339 L219,326 Z";
